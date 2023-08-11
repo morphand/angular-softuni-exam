ad467533e541e0cacb152bfd8a0b34a02015d50e
morphand <75131830+morphand@users.noreply.github.com>
2023-08-11 11:08:31 +0300

    navbar changes depending on the state of the user auth

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  auth$: Observable<string>;
+  isUserLoggedIn: boolean = false;
+  constructor(private store: Store<{ auth: string }>) {
+    this.auth$ = this.store.select('auth');
+  }
 
+  ngOnInit(): void {
+    this.auth$.subscribe((data) => {
+      this.isUserLoggedIn = data === 'login';
+    });
+  }
 }
