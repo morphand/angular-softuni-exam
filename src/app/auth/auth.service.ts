93f27fc69361363c0d2196eb4e3a75bbad0845d0
morphand <75131830+morphand@users.noreply.github.com>
2023-08-11 14:50:34 +0300

    initial auth service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { API_URL, LOGIN_ENDPOINT } from 'src/utils/constants';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  requestLogin(email: String | null, password: String | null) {
+    return this.http.post<LoginResult>(`${API_URL}${LOGIN_ENDPOINT}`, {
+      username: email,
+      password: password,
+    });
+  }
+
+  setToken(token: JSONWebToken) {
+    localStorage.setItem('auth', token);
+    return this.isTokenAvailable();
+  }
+
+  getToken() {
+    return localStorage.getItem('auth');
+  }
+
+  isTokenAvailable() {
+    return Boolean(this.getToken());
+  }
+}
