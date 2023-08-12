3f67cb0b80b93334a4f9a276fbe72b5ed43006e5
morphand <75131830+morphand@users.noreply.github.com>
2023-08-12 20:14:54 +0300

    add getCarById to the catalog service

--- src/app/core/catalog/catalog.service.ts
+++ src/app/core/catalog/catalog.service.ts
@@ -1,7 +1,11 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { API_URL, CATALOG_ENDPOINT } from 'src/utils/constants';
+import {
+  API_URL,
+  CATALOG_ENDPOINT,
+  CATALOG_SINGLE_CAR_ENDPOINT,
+} from 'src/utils/constants';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +15,9 @@ export class CatalogService {
 
   getCatalog() {
     return this.http.get<CatalogResult>(`${API_URL}${CATALOG_ENDPOINT}`);
+  }
+
+  getCarById(carId: string) {
+    return this.http.get<SingleCarResult>(`${API_URL}${CATALOG_SINGLE_CAR_ENDPOINT}${carId}`);
   }
 }
