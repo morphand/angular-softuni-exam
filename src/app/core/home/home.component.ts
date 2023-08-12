a852d634f7c4dca16fada216e3dffecdb543a2f3
morphand <75131830+morphand@users.noreply.github.com>
2023-08-12 21:15:56 +0300

    add animations to the home component

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+const slideUp = trigger('slideUp', [
+  transition(':enter', [
+    style({
+      transform: 'translateY(3rem)',
+    }),
+    animate('500ms ease-in', style({ transform: 'translateY(0)' })),
+  ]),
+]);
+const slideLeft = trigger('slideLeft', [
+  transition(':enter', [
+    style({
+      transform: 'translateX(3rem)',
+    }),
+    animate('300ms ease-in', style({ transform: 'translateX(0)' })),
+  ]),
+]);
+const slideDown = trigger('slideDown', [
+  transition(':enter', [
+    style({
+      transform: 'translateY(-9rem)',
+    }),
+    animate('350ms ease-in', style({ transform: 'translateZ(0)' })),
+  ]),
+]);
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  animations: [slideUp, slideLeft, slideDown],
 })
 export class HomeComponent {}
